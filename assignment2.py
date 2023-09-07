32771d297bfef8ba0558e050fdbab8472a39a9bd
Maverick Jude Lenon <143677915+MerickJL@users.noreply.github.com>
2023-09-07 14:31:16 -0400

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,68 @@
+import socket
+
+class assignment2:
+    def __init__(self, year):
+        self.year = year
+
+    def tellAge(self, currentYear):
+        age = currentYear - self.year
+        print(f'Your age is {age}')
+
+    def listAnniversaries(self):
+        #Current Year Assumption
+        current_year = 2023
+        anniversaries = [i for i in range(10, current_year - self.year + 1, 10)]
+        return anniversaries
+
+    def modifyYear(self, n):
+        year_str = str(self.year)
+        repeated_str = year_str[:2] * n
+        odd_chars = ''.join(str(self.year * n)[::2])
+        modified_str = repeated_str + odd_chars
+        return modified_str
+
+    @staticmethod
+    def checkGoodString(string):
+        if len(string) >= 9 and string[0].islower() and sum(c.isdigit() for c in string) == 1:
+            return True
+        return False
+
+    @staticmethod
+    def connectTcp(host, port):
+        try:
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((host, port))
+            client_socket.close()
+            return True
+        except Exception as e:
+            return False
+
+
+# Easy Change Testing Varaiables:
+# Task 2 (Age)
+a = assignment2(2000)
+a.tellAge(2022)
+
+# Task 3 (List)
+a = assignment2(2000)
+ret = a.listAnniversaries()
+print(ret)
+
+# Task 4 (String Manipulation)
+a = assignment2(2000)
+ret = a.modifyYear(5)
+print(ret)
+
+# Task 5 (Loop and Conditional statements)
+ret = assignment2.checkGoodString("f1obar0more")
+print(ret)
+
+ret = assignment2.checkGoodString("foobar0more")
+print(ret)
+
+# Task 6 (Socket)
+retval = assignment2.connectTcp("www.google.com", 80)
+if retval:
+    print("Connection established correctly")
+else:
+    print("Some error")
